66845ef62ad5d770f1f6902b887403193bbac944
Aryan2205974 <aryan2205974@gmail.com>
2026-06-16 16:10:50 +0530

    Allow any Netlify domain to connect via CORS

--- api/main.py
+++ api/main.py
@@ -32,8 +32,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
         "https://tendergeneration.netlify.app"
     ],
+    allow_origin_regex="https://.*\\.netlify\\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
